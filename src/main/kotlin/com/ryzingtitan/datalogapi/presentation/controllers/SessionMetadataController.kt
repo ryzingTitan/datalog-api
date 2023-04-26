de82b987d691c18919e49cffae08c0a7e4cc8fab
ryzingTitan <computerwhiz33@gmail.com>
2023-04-26 00:36:49 -0400

    Updating CORS settings

--- src/main/kotlin/com/ryzingtitan/datalogapi/presentation/controllers/SessionMetadataController.kt
+++ src/main/kotlin/com/ryzingtitan/datalogapi/presentation/controllers/SessionMetadataController.kt
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@CrossOrigin(origins = ["http://localhost:3000"])
+@CrossOrigin(origins = ["http://localhost:3000", "https://datalog-viewer-uonahdb5jq-uc.a.run.app"])
 @RequestMapping(path = ["/api/sessions/"])
 class SessionMetadataController(private val sessionMetadataService: SessionMetadataService) {
     private val logger = LoggerFactory.getLogger(SessionMetadataController::class.java)
